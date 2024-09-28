dd0da8a89ccd9b40b09aec9790257d497457a15d
LemurPwned <jakub.mojsiejuk@gmail.com>
2024-09-28 15:34:09 +0200

    adding docker compose prod and fixing up the qwen prompt

--- backend/taxchatter/chat/llm_prompts/qwen.py
+++ backend/taxchatter/chat/llm_prompts/qwen.py
@@ -2,6 +2,7 @@ import base64
 import io
 import json
 import os
+import tempfile
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -24,11 +25,12 @@ Podaj dane w poniższym formacie:
     "miejsce_zawracia_umowy": "Twoja odpowiedz",
     "data_zawarcia_umowy": "Twoja odpowiedź"
 }
-Nie odbiegaj od powyższego formatu. W razie braku informacji, pozostaw pole puste
+Nie odbiegaj od powyższego formatu. Gdy danej informacji nie ma w obrazie, pozostaw pole puste.
+Nie próbuj wypełniać pola, jeżeli nie masz odpowiedniej wystarczająco informacji.
 """
 
 
-def submit_pdf(image_path, img_size: int = 900):
+def get_qwen_deployment():
     openai_api_key = os.getenv("RUNPOD_API_KEY")
     openai_api_base = "https://ep2z4vudsnqp7g-8000.proxy.runpod.net/v1"
     # min_pixels=32 * 28 * 28, max_pixels=1920 * 28 * 28
@@ -36,6 +38,56 @@ def submit_pdf(image_path, img_size: int = 900):
         api_key=openai_api_key,
         base_url=openai_api_base,
     )
+    return client
+
+
+def get_extract_messages(base64_qwen: str):
+    return [
+        {
+            "role": "system",
+            "content": "Jesteś OCRem do czytania polskiego tekstu z obrazków. Specjalizujesz się w czytaniu pisma "
+            "odręcznego, nawet jeżeli jest bardzo trudne do odczytania.",
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {"url": base64_qwen},
+                    "min_pixels": 32 * 28 * 28,
+                    "max_pixels": 1920 * 28 * 28,
+                },
+                {
+                    "type": "text",
+                    "text": "Wciągnij z tekstu dane dotyczące umowy. " + OUTPUT_FORMAT_SYS,
+                },
+            ],
+        },
+    ]
+
+
+def get_ocr_chat_messages(base64_qwen: str):
+    return [
+        {
+            "role": "system",
+            "content": "Jesteś OCRem do czytania polskiego tekstu z obrazków. Specjalizujesz się w czytaniu pisma "
+            "odręcznego, nawet jeżeli jest bardzo trudne do odczytania.",
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {"url": base64_qwen},
+                    "min_pixels": 32 * 28 * 28,
+                    "max_pixels": 1920 * 28 * 28,
+                }
+            ],
+        },
+    ]
+
+
+def preproc_img(image_path, img_size: int = 900):
     with open(image_path, "rb") as f:
         image = Image.open(f)
         resized_image = image.resize((img_size, img_size))
@@ -43,32 +95,39 @@ def submit_pdf(image_path, img_size: int = 900):
         resized_image.save(buffered, format="JPEG")
         encoded_image = base64.b64encode(buffered.getvalue())
     encoded_image_text = encoded_image.decode("utf-8")
-    base64_qwen = f"data:image;base64,{encoded_image_text}"
-    chat_response = client.chat.completions.create(
-        model="Qwen/Qwen2-VL-7B-Instruct",
-        messages=[
-            {
-                "role": "system",
-                "content": "Jesteś OCRem do czytania polskiego tekstu z obrazków. Specjalizujesz się w czytaniu pisma "
-                "odręcznego, nawet jeżeli jest bardzo trudne do odczytania.",
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": base64_qwen},
-                        "min_pixels": 32 * 28 * 28,
-                        "max_pixels": 1920 * 28 * 28,
-                    },
-                    {
-                        "type": "text",
-                        "text": "Wciągnij z tekstu dane dotyczące umowy. " + OUTPUT_FORMAT_SYS,
-                    },
-                ],
-            },
-        ],
-    )
+    return f"data:image;base64,{encoded_image_text}"
+
+
+def ocr_pdf(pdf_path: str, img_size: int = 900, pdf_dpi: int = 500):
+    pages = convert_from_path(pdf_path, pdf_dpi)
+    ocr_pages = []
+    for _, page in enumerate(pages):
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            page.save(temp_file, "JPEG")
+            response = submit_image(temp_file.name, img_size, get_ocr_chat_messages)
+            ocr_pages.append(response)
+
+
+def extract_pdf(pdf_path: str, img_size: int = 900, pdf_dpi: int = 500):
+    pages = convert_from_path(pdf_path, pdf_dpi)
+    ocr_pages = []
+    for _, page in enumerate(pages):
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            page.save(temp_file, "JPEG")
+            response = submit_image(temp_file.name, get_extract_messages)
+            ocr_pages.append(response)
+    return ocr_pages
+
+
+def submit_image(
+    image_path,
+    img_msg: callable,
+    img_size: int = 900,
+):
+    client = get_qwen_deployment()
+    base64_qwen = preproc_img(image_path, img_size)
+
+    chat_response = client.chat.completions.create(model="Qwen/Qwen2-VL-7B-Instruct", messages=img_msg(base64_qwen))
     # print("Chat response:", chat_response)
     resp = chat_response.choices[0].message.content
     try:
@@ -79,12 +138,13 @@ def submit_pdf(image_path, img_size: int = 900):
     return resp
 
 
-def extract_pdf_info(path: str):
-    pages = convert_from_path(path, 500)
-    for count, page in enumerate(pages):
-        page.save(f"umow_{count}.jpg", "JPEG")
-
-
 if __name__ == "__main__":
     # extract_pdf_info("/Users/jm/repos/tax-chatterbox/Umowa pożyczki - wzór.pdf")
-    print(submit_pdf("/Users/jm/repos/tax-chatterbox/umow_0.jpg"))
+    # print(submit_image("/Users/jm/repos/tax-chatterbox/umow_0.jpg"))
+    print(
+        ocr_pdf(
+            "/Users/jm/repos/tax-chatterbox/Umowa pożyczki - wzór.pdf",
+            img_size=900,
+            pdf_dpi=500,
+        )
+    )
