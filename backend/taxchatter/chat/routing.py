f41f681342a252a57cd20ce1c3a41b19291a1959
wwydmanski <wwydmanski@gmail.com>
2024-09-28 15:13:14 +0200

    feat: add basic chat websocket integration

--- backend/taxchatter/chat/routing.py
+++ backend/taxchatter/chat/routing.py
@@ -1,7 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/ai/$', consumers.AIConsumer.as_asgi()),
+    path('ws/v1/chat', consumers.AIConsumer.as_asgi()),
 ]
