ee1244e7e164d032f782abb237317b3c25579206
LemurPwned <jakub.mojsiejuk@gmail.com>
2024-09-28 16:41:07 +0200

    port change

--- backend/taxchatter/chat/consumers.py
+++ backend/taxchatter/chat/consumers.py
@@ -1,10 +1,10 @@
 import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
-from asgiref.sync import sync_to_async
-from openai import AsyncOpenAI
 from loguru import logger
+
 from . import chat_utils
+
 
 class AIConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -15,10 +15,13 @@ class AIConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json["message"]
         logger.info(f"Received message: {message}")
 
         # Send message to AI consumer
-        answer = await chat_utils.get_ai_response(message, callback=lambda x: self.send(text_data=json.dumps({'message': x, "command": "basicFlowPartial"})))
+        answer = await chat_utils.get_ai_response(
+            message,
+            callback=lambda x: self.send(text_data=json.dumps({"message": x, "command": "basicFlowPartial"})),
+        )
         logger.info(f"AI response: {answer}")
-        await self.send(text_data=json.dumps({'message': answer, "command": "basicFlowComplete"}))
+        await self.send(text_data=json.dumps({"message": answer, "command": "basicFlowComplete"}))
